c329fc6fd842e2d733d7ff3d2bd388185b665ec8
Bileonaire <kiokoleon@gmail.com>
2021-12-14 11:45:19 +0300

    Add Dynamic Income

--- src/components/IncomeExpenses.js
+++ src/components/IncomeExpenses.js
@@ -1,14 +1,31 @@
+import React, { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
+import { moneyFormatter } from '../utils';
+
 export const IncomeExpenses = () => {
-    return (
-        <div className="inc-exp-container">
-            <div>
-                <h4>Income</h4>
-                <p className="money plus">+$0.00</p>
-            </div>
-            <div>
-                <h4>Expense</h4>
-                <p className="money minus">-$0.00</p>
-            </div>
+  const { transactions } = useContext(GlobalContext);
+
+  const amounts = transactions.map(transaction => transaction.amount);
+
+  const income = amounts
+    .filter(item => item > 0)
+    .reduce((acc, item) => (acc += item), 0);
+
+  const expense = (
+    amounts.filter(item => item < 0).reduce((acc, item) => (acc += item), 0) *
+    -1
+  );
+
+  return (
+    <div className="inc-exp-container">
+        <div>
+          <h4>Income</h4>
+  <p className="money plus">{moneyFormatter(income)}</p>
+        </div>
+        <div>
+          <h4>Expense</h4>
+  <p className="money minus">{moneyFormatter(expense)}</p>
+        </div>
       </div>
-    )
+  )
 }
