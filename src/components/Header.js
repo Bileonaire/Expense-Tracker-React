725db118a55577adfb48d45a5402009cee39c93f
Bileonaire <kiokoleon@gmail.com>
2021-12-14 11:51:22 +0300

    Update Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Header = () => {
     return (
         <>
-           <h2>Expense Tracker</h2>
+           <h2>Bileonaire Expense Tracker</h2>
         </>
     )
 }
